afb80d005af4b53b78d3fdd3899c38120f149ab7
Luthaf <luthaf@luthaf.fr>
2017-05-26 22:11:58 +0200

    Add iteration

---
+++ tests/iter.rs
@@ -0,0 +1,54 @@
+#[macro_use]
+extern crate soa_derive;
+mod particles;
+
+use particles::{Particle, ParticleVec};
+
+#[test]
+fn iter() {
+    let mut particles = ParticleVec::new();
+    particles.push(Particle::new(String::from("Na"), 56.0));
+    particles.push(Particle::new(String::from("Cl"), 56.0));
+    particles.push(Particle::new(String::from("Zn"), 56.0));
+
+    let mut iter = particles.iter();
+    assert_eq!(iter.next().unwrap().name, "Na");
+    assert_eq!(iter.next().unwrap().name, "Cl");
+    assert_eq!(iter.next().unwrap().name, "Zn");
+
+    assert!(iter.next().is_none());
+
+    let slice = particles.as_slice();
+    let mut iter = slice.iter();
+    assert_eq!(iter.next().unwrap().name, "Na");
+    assert_eq!(iter.next().unwrap().name, "Cl");
+    assert_eq!(iter.next().unwrap().name, "Zn");
+
+    assert!(iter.next().is_none());
+}
+
+#[test]
+fn iter_mut() {
+    let mut particles = ParticleVec::new();
+    particles.push(Particle::new(String::from("Na"), 0.0));
+    particles.push(Particle::new(String::from("Cl"), 0.0));
+    particles.push(Particle::new(String::from("Zn"), 0.0));
+
+    for particle in particles.iter_mut() {
+        *particle.mass += 1.0;
+    }
+    assert_eq!(particles.mass[0], 1.0);
+    assert_eq!(particles.mass[1], 1.0);
+    assert_eq!(particles.mass[2], 1.0);
+
+    {
+        let mut slice = particles.as_mut_slice();
+        for particle in slice.iter_mut() {
+            *particle.mass += 1.0;
+        }
+    }
+
+    assert_eq!(particles.mass[0], 2.0);
+    assert_eq!(particles.mass[1], 2.0);
+    assert_eq!(particles.mass[2], 2.0);
+}
