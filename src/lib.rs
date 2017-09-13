42dfc34e4d89baeb06f1974648e29b1771e795e7
Luthaf <luthaf@luthaf.fr>
2017-09-14 00:29:12 +0200

    Update the main documentation to talk about zip

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,21 @@
 //! automatically generate code from a given struct `T` that allow to replace
 //! `Vec<T>` with a struct of arrays. For example, the following code
 //!
-//! ```ignore
+//! ```
+//! # #[macro_use] extern crate soa_derive;
+//! # fn main() {
 //! #[derive(StructOfArray)]
 //! pub struct Particle {
 //!     pub x: f64,
 //!     pub y: f64,
 //!     pub z: f64,
 //! }
+//! # }
 //! ```
 //!
 //! will generate a `ParticleVec` struct that looks like this:
 //!
-//! ```ignore
+//! ```
 //! pub struct ParticleVec {
 //!     pub x: Vec<f64>,
 //!     pub y: Vec<f64>,
@@ -34,7 +37,9 @@
 //! traits (such as `Debug` or `PartialEq`), you can add an attribute
 //! `#[soa_derive = "Debug, PartialEq"]` to the struct declaration.
 //!
-//! ```ignore
+//! ```
+//! # #[macro_use] extern crate soa_derive;
+//! # fn main() {
 //! #[derive(Debug, PartialEq, StructOfArray)]
 //! #[soa_derive = "Debug, PartialEq"]
 //! pub struct Particle {
@@ -42,6 +47,72 @@
 //!     pub y: f64,
 //!     pub z: f64,
 //! }
+//! # }
+//! ```
+//!
+//! # `zip` functions
+//!
+//! You can activate generation of two additional functions `zip` and
+//! `zip_mut` by decorating some fields of the struct with
+//! `#[soa_derive(zip)]`. These functions allow to iterate over multiple
+//! fields of the derivated `Vec`.
+//!
+//! ```
+//! # #[macro_use] extern crate soa_derive;
+//! # fn main() {
+//! #[derive(StructOfArray)]
+//! pub struct Particle {
+//!     #[soa_derive(zip)]
+//!     pub x: f64,
+//!     #[soa_derive(zip)]
+//!     pub y: f64,
+//!     #[soa_derive(zip)]
+//!     pub z: f64,
+//!     // Do not derive zip for this field
+//!     pub name: String,
+//! }
+//! # }
+//! ```
+//!
+//! This generate a `zip_particle` module containing marker types used to
+//! request some specific data, like this:
+//!
+//! ```
+//! # #[macro_use] extern crate soa_derive;
+//! # #[derive(StructOfArray)]
+//! # pub struct Particle {
+//! #    #[soa_derive(zip)]
+//! #    pub x: f64,
+//! #    #[soa_derive(zip)]
+//! #    pub y: f64,
+//! #    #[soa_derive(zip)]
+//! #    pub z: f64,
+//! #    // Do not derive zip for this field
+//! #    pub name: String,
+//! # }
+//! # fn main() {
+//! use zip_particle::{X, Y, Z};
+//!
+//! let mut vector = ParticleVec::new();
+//!
+//! for x in vector.zip(&X) {
+//!     // x is a &f64
+//! }
+//!
+//! let mut total = 0.0;
+//! for (&x, &z) in vector.zip((&X, &Z)) {
+//!     total += x * z;
+//! }
+//!
+//! // Mutable iteration is also available with zip_mut
+//! for y in vector.zip_mut(&mut Y) {
+//!     *y += 6.4;
+//! }
+//!
+//! for (&x, z) in vector.zip_mut((&X, &mut Z)) {
+//!     *z *= x;
+//! }
+//! # }
 //! ```
 //!
 //! # Caveats and limitations
@@ -57,6 +128,12 @@
 //! This means that the we can not index into a `CheeseVec`, and that a few
 //! functions are duplicated, or require a call to `as_ref()/as_mut()` to
 //! change the type used.
+//!
+//! Because of the way the code is implemented, the `zip` functions can not be
+//! derived on more than 4 fields, as the compilation time grows exponentialy.
+//! Deriving it for 5 fields makes the compilation time goes from 6s to more
+//! than 2 minutes. These functions will only be derived if the struct is
+//! declared as public.
 //!
 //! # Documentation
 //!
