0104d44739e9ca644e5bfd0d54257015ea9332cd
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-12-01 10:32:46 +0000

    Implement zip as a macro_rules
    This removes the limitation on the number of zippable fields

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,51 @@
 extern crate soa_derive_internal;
 
 pub use soa_derive_internal::*;
+
+#[macro_export]
+macro_rules! soa_zip {
+    // @flatten creates a tuple-flattening closure for .map() call
+    // Finish recursion
+    (@flatten $p:pat => $tup:expr ) => {
+        |$p| $tup
+    };
+    // Eat an element ($_iter) and add it to the current closure. Then recurse
+    (@flatten $p:pat => ( $($tup:tt)* ) , $_iter:expr $( , $tail:expr )* ) => {
+        soa_zip!(@flatten ($p, a) => ( $($tup)*, a ) $( , $tail )*)
+    };
+
+    // The main code is emmited here: we create an iterator, zip it and then
+    // map the zipped iterator to flatten it
+    (@final , $first: expr, $($tail: expr,)*) => {
+        ::std::iter::IntoIterator::into_iter($first)
+            $(
+                .zip($tail)
+            )*
+            .map(
+                soa_zip!(@flatten a => (a) $( , $tail )*)
+            )
+    };
+
+    // Eat the last `mut $field` and then emit code
+    (@munch $self: expr, {mut $field: ident} -> [$($output: tt)*] $($ext: expr ,)*) => {
+        soa_zip!(@final $($output)*, $self.$field.iter_mut(), $($ext, )*)
+    };
+    // Eat the last `$field` and then emit code
+    (@munch $self: expr, {$field: ident} -> [$($output: tt)*] $($ext: expr ,)*) => {
+        soa_zip!(@final $($output)*, $self.$field.iter(), $($ext, )*)
+    };
+
+    // Eat the next `mut $field` and then recurse
+    (@munch $self: expr, {mut $field: ident, $($tail: tt)*} -> [$($output: tt)*] $($ext: expr ,)*) => {
+        soa_zip!(@munch $self, {$($tail)*} -> [$($output)*, $self.$field.iter_mut()] $($ext, )*)
+    };
+    // Eat the next `$field` and then recurse
+    (@munch $self: expr, {$field: ident, $($tail: tt)*} -> [$($output: tt)*] $($ext: expr ,)*) => {
+        soa_zip!(@munch $self, {$($tail)*} -> [$($output)*, $self.$field.iter()] $($ext, )*)
+    };
+
+    // The macro entry point
+    ($self: expr, [$($tail: tt)*] $(, $ext: expr)* $(,)*) => {
+        soa_zip!(@munch $self, {$($tail)*} -> [] $($ext ,)*)
+    };
+}
