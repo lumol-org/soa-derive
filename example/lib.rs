f7525255b50bce95f725b7dc1674320ec100f030
Joshua Klein <mobiusklein@gmail.com>
2025-01-16 15:19:34 -0500

    docs

--- example/lib.rs
+++ example/lib.rs
@@ -44,6 +44,7 @@ extern crate soa_derive;
 /// A basic Particle type
 #[derive(Debug, PartialEq, StructOfArray)]
 #[soa_derive(Debug, PartialEq)]
+#[generate_traits]
 pub struct Particle {
     /// Mass of the particle
     pub mass: f64,
