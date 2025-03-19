103a80d4b08361a2bf0262973d6c3d254dd5bac5
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-03-19 23:22:33 +0100

    Move generic tests to generic.rs

--- tests/generic.rs
+++ tests/generic.rs
@@ -1,25 +1,24 @@
 mod particles;
 use std::marker::PhantomData;
+use std::fmt::Debug;
 
 use particles::ParticleVec;
-use soa_derive::{SoAVec, StructOfArray};
+use soa_derive::{SoAVec, SoASlice, StructOfArray};
 
 use self::particles::Particle;
 
 fn may_iter<T: StructOfArray, V: SoAVec<T>>(vec: &V) -> V::Iter<'_> {
-    let x=  vec.iter();
-    x
+    vec.iter()
 }
 
 fn may_push<T: StructOfArray, V: SoAVec<T>>(vec: &mut V, val: T) {
-    vec.push(val);
+    vec.push(val)
 }
 
 fn may_sort_generic<T: StructOfArray, V: SoAVec<T>>(vec: &mut V) where for<'t> V::Ref<'t> : PartialOrd {
     let mut indices: Vec<_> = (0..vec.len()).collect();
 
     indices.sort_by(|j, k| {
-
         let a = vec.index(*j);
         let b = vec.index(*k);
         a.partial_cmp(&b).unwrap()
@@ -73,36 +72,44 @@ fn test_generic_type_behavior() {
 
 
 #[derive(Debug, Clone)]
-struct Swarm<T: StructOfArray, V: SoAVec<T>> {
-    entries: V,
-    _t: PhantomData<T>
+struct VecWrap<T: StructOfArray, V: SoAVec<T>> {
+    data: V,
+    marker: PhantomData<T>
 }
 
-impl<T: StructOfArray, V: SoAVec<T>> Swarm<T, V> {
+impl<T: StructOfArray, V: SoAVec<T>> VecWrap<T, V> {
     fn new() -> Self {
         Self {
-            entries: V::new(),
-            _t: PhantomData
+            data: V::new(),
+            marker: PhantomData
         }
     }
 
     fn push(&mut self, value: T) {
-        self.entries.push(value);
+        self.data.push(value);
     }
 
     fn iter(&self) -> V::Iter<'_> {
-        self.entries.iter()
+        self.data.iter()
     }
 
     fn view(&self) -> V::Slice<'_> {
-        self.entries.as_slice()
+        self.data.as_slice()
+    }
+
+    fn first(&self) -> Option<V::Ref<'_>> {
+        self.data.first()
+    }
+
+    fn sort_by<F>(&mut self, f: F) where F: FnMut(V::Ref<'_>, V::Ref<'_>) -> std::cmp::Ordering {
+        self.data.sort_by(f);
     }
 }
 
 #[test]
 fn test_wrapped() {
-    let mut this: Swarm<Particle, ParticleVec> = Swarm::new();
-    let x= Particle::new("foo".into(), 100.0);
+    let mut this: VecWrap<Particle, ParticleVec> = VecWrap::new();
+    let x = Particle::new("foo".into(), 100.0);
     this.push(x);
     let x = Particle::new("bar".into(), 1000.0);
     this.push(x);
@@ -110,6 +117,74 @@ fn test_wrapped() {
     this.push(x);
 
     assert_eq!(this.iter().count(), 3);
-
     assert_eq!(this.view().len(), 3);
+}
+
+fn iter_max_generic<'a, T: StructOfArray, V: SoASlice<T> + 'a>(vec: &'a V) -> Option<V::Ref<'a>>
+where
+    V::Ref<'a>: PartialOrd + Debug
+{
+    let x= vec.iter().reduce(|a, b| {
+        if a.partial_cmp(&b).unwrap().is_ge() {
+            a
+        } else {
+            b
+        }
+    });
+    x
+}
+
+fn iter_max_generic_iter<'a, T: StructOfArray, V: SoAVec<T>>(it: V::Iter<'a>) -> Option<V::Ref<'a>>
+where
+    V::Ref<'a>: PartialOrd
+{
+    it.reduce(|a: V::Ref<'_>, b: V::Ref<'_>| {
+        if a.partial_cmp(&b).unwrap().is_ge() {
+            a
+        } else {
+            b
+        }
+    })
+}
+
+fn slice_ref_len<T: StructOfArray, V: SoAVec<T>>(vec: &V) -> usize {
+    let view = vec.as_slice();
+    let n = view.iter().count();
+    assert_eq!(view.into_iter().count(), n);
+    n
+}
+
+
+#[test]
+fn test_ops() {
+    let mut vec = ParticleVec::new();
+    vec.push(Particle::new("foo".into(), 100.0));
+    vec.push(Particle::new("bar".into(), 1000.0));
+    vec.push(Particle::new("baz".into(), 50.0));
+    // let x = iter_max_generic(&view);
+    // eprintln!("{x:?}");
+    let y = iter_max_generic_iter::<Particle, ParticleVec>(vec.iter());
+    eprintln!("{y:?}");
+    let k = slice_ref_len(&vec);
+    assert_eq!(k, 3);
+
+    let mut view = vec.as_mut_slice();
+    view.iter_mut().for_each(|f| {
+        *f.mass *= 2.0;
+    });
+
+    let view = vec.as_slice();
+    let z = iter_max_generic(&view).unwrap();
+    assert_eq!(z.name, "foo");
+
+    let n = view.iter().count();
+    assert!(n > 0);
+
+    let mut vec = VecWrap::<Particle, ParticleVec>::new();
+    vec.push(Particle::new("foo".into(), 100.0));
+    vec.push(Particle::new("bar".into(), 1000.0));
+    vec.push(Particle::new("baz".into(), 50.0));
+    vec.sort_by(|a, b| a.mass.total_cmp(b.mass));
+
+    assert_eq!(vec.first().unwrap().name, "baz");
 }
