4dd8493ee6a7da20d14b6c260deec3653223f242
eranfu <1119369173@qq.com>
2025-04-13 13:51:09 +0800

    Fix the conflict with iterator_tools.

--- tests/particles/mod.rs
+++ tests/particles/mod.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
-
 use soa_derive::*;
+use itertools::Itertools;
 
 #[derive(Debug, Clone, PartialOrd, PartialEq, StructOfArray)]
 #[soa_derive(Debug, Clone, PartialOrd, PartialEq)]
@@ -23,4 +23,11 @@ impl ParticleVec {
         self.name.extend_from_slice(&other.name);
         self.mass.extend_from_slice(&other.mass);
     }
+}
+
+#[test]
+fn use_iterator_tools_get() {
+    let vec = ParticleVec::new();
+    let particle = vec.iter().get(..0).find_or_first(|_|true);
+    assert_eq!(particle, None);
 }
